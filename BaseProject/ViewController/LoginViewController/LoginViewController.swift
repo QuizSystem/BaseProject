6b655600f8a40b290c36ea104955d45e2a97a794
Nguyen Van Thieu B <nguyen.van.thieub@tms-Mac-mini.local>
2017-03-01 16:35:02 +0700

    Use Facebook SDK

--- BaseProject/ViewController/LoginViewController/LoginViewController.swift
+++ BaseProject/ViewController/LoginViewController/LoginViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
 
 class LoginViewController: BaseViewController {
+    
+    let loginManager = FBSDKLoginManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +25,28 @@ class LoginViewController: BaseViewController {
     @IBAction func loginButtonClicked(_ sender: Any) {
         let mainViewController = MainViewController()
         self.navigationController?.pushViewController(mainViewController, animated: true)
+    }
+    
+    @IBAction func loginFacebookButtonClicked(_ sender: Any) {
+        if (FBSDKAccessToken.current() != nil) {
+            // TODO: do something when have Access Token of Facebook
+            return
+        }
+        else {
+            loginManager.logIn(withReadPermissions: ["public_profile", "email", "user_friends"], from: self.parent) {[weak self] (result, error) in
+                if error != nil || (result?.isCancelled)! {
+                    self?.showAlert(title: "Error", password: "Failure")
+                } else {
+                    self?.showAlert(title: "Ok", password: "Success")
+                }
+            }
+        }
+    }
+    
+    func showAlert(title: String, password: String) {
+        let alert = UIAlertController(title: title, message: password, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        navigationController?.present(alert, animated: true, completion: nil)
     }
     
 }
